98e24bb1d41ee8ecf86e894bf39b314c58559188
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-10-23 13:06:23 -0600

    Registro de idioma para el pipe de date en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
+// Configuración de locale de la app
+import localeEsCR from '@angular/common/locales/es-CR';
+import localeFrCA from '@angular/common/locales/fr-CA';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEsCR)
+registerLocaleData(localeFrCA)
 
 @NgModule({
   declarations: [
@@ -16,7 +24,9 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'es-CR'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
