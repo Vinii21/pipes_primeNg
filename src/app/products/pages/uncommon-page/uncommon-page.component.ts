4cb89bc71c91d0419d5f119d2565ba5a720d1a89
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-10-27 10:24:18 -0600

    Pipes de angular

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from "rxjs";
 
 @Component({
   selector: 'app-uncommon-page',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./uncommon-page.component.css']
 })
 export class UncommonPageComponent {
+
+  // i18n select
+  public name: string = "Vinicio";
+  public gender: "male" | "female" = "male";
+  public invitationMap = {
+    "male": "invitarlo",
+    "female": "invitarla",
+  }
+
+  changeClient():void {
+    this.name = "Melissa"
+    this.gender = "female"
+  }
+
+  // i18Plural
+  public clients: string[] = ["Francisco", "Amelia", "Vanesa", "Kate"];
+
+  public clientsMap = {
+    "=0": "no tenemos ningún cliente esperando.",
+    "=1": "tenemos 1 cliente esperando.",
+    "=2": "tenemos 2 clientes esperando.",
+    "other": "tenemos # clientes esperando.",
+  }
+
+  deleteClient():void {
+    this.clients.shift();
+  }
+
+  // KeyValue Pipe
+  public person = {
+    name: "Vinicio",
+    age: 25,
+    address: "Sarchi"
+  }
+
+  // async pipe
+  public myObservalTimer = interval(2000);
 
 }
